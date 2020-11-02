844c795ca53f8bd638be5388f3d67aa4ccc4da3b
Md Nurul Hasan <mdnurul.hasan@cogitosoftware.in>
2020-11-02 21:58:34 +0530

    Uploaded code on 02112020

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 
-$route['default_controller'] = "Frontend/Home";
+$route['default_controller'] = "Home";
 
 $route['admin'] = "login";
 $route['404_override'] = 'error_404';
